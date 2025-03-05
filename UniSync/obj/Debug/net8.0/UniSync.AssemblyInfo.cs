bb8742c08e837da3ee926d41f410089a0f780da1
Artur Huliiev <artur.huliiev@oa.edu.ua>
2025-03-05 15:45:36 +0200

    repeated initial commit

--- UniSync/obj/Debug/net8.0/UniSync.AssemblyInfo.cs
+++ UniSync/obj/Debug/net8.0/UniSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3b089a0a18dda8248f6635e1af39e11f66946b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
